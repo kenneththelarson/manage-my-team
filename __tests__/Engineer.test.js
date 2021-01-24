cac3b7886891e21d13abfe21d2700aa7140ec5d4
Kenny <kenneththelarson@gmail.com>
2021-01-24 16:37:38 -0700

    engineer and intern tests pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,15 @@
+const { expect } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
+
+jest.mock('../lib/Engineer.js');
+
+test('creates an engineer object', () => {
+    const engineer = new Engineer();
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.officeNumber).toEqual(expect.any(Number));
+    expect(engineer.role).toEqual(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
+});
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,16 @@
 const Intern = require('../lib/Intern');
+
+jest.mock('../lib/Intern.js');
+
+test('creates an intern object', () => {
+    const intern = new Intern();
+
+    expect(intern.name).toEqual(expect.any(String));
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.any(String));
+    expect(intern.officeNumber).toEqual(expect.any(Number));
+    expect(intern.role).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
+
+    console.log(intern);
+});
