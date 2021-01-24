6eb51072e697bc65f70338a75c2a5dbfbb9cb371
Kenny <kenneththelarson@gmail.com>
2021-01-24 15:03:42 -0700

    manager test pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,16 @@
+const { expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
+
+jest.mock('../lib/Manager.js');
+
+test('creates a manager object', () => {
+    const manager = new Manager();
+
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+    expect(manager.role).toEqual(expect.any(String));
+
+    console.log(manager.role);
+});
